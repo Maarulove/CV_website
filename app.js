bf17044c2cdccb149f176550621cd765998908ff
Maarulove <qudayev62@mail.ru>
2023-09-23 23:44:00 +0200

    modified feedback

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+
+
 function send() {
     let feedback = document.getElementById("feedback").value;
     let name = document.getElementById("name").value
@@ -6,9 +9,23 @@ function send() {
         document.getElementById("empty_field").innerText = "Please make sure that you provide both name and feedback"
     }
     else {
-        
-        document.getElementById("empty_field").innerText = ""
-        document.getElementById("result").innerText = ""
         document.getElementById("result").innerText = `${name}: ${feedback}`;
+        document.getElementById("feedback").value = "";
+        document.getElementById("empty_field").innerText = ""
+
+        var agree = document.getElementById("is_agree")
+        agree.addEventListener("change", function()  {
+            if (agree.checked) {
+                alert ("Please make sure you agreed with privacy terms")
+            }
+        })
+        
     }
+
 }
+function clear_input(){
+    document.getElementById("name").value = ""
+
+}
+
+        
